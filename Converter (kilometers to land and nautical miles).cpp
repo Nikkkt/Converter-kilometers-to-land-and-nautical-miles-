7029e5f88775444a922aa051a8385758be095dfe
NTer <NTer@MACHINE-LGU4SHC>
2022-04-24 00:43:58 +0300

    Добавьте файлы проекта.

---
+++ Converter (kilometers to land and nautical miles).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float kilometers, l_miles, n_miles;
+
+    cout << "This program converts kilometers to land and nautical miles" << endl;
+
+    cout << "Enter how many kilometers: ";
+    cin >> kilometers;
+
+    l_miles = kilometers / 1.609;
+    n_miles = kilometers / 1.852;
+
+    cout << "In " << kilometers << " kilometers there is " << l_miles << " land miles" << endl;
+    cout << "In " << kilometers << " kilometers there is " << n_miles << " nautical miles" << endl;
+}
